e541b46973fe972dd10ac08bed314aada2f04022
Shane Jaroch <chown_tee@proton.me>
2026-06-30 14:06:10 -0400

    fixup prune mixins from other branch

--- benches/bench_cmp_iter_lattice.rs
+++ benches/bench_cmp_iter_lattice.rs
@@ -17,7 +17,10 @@ struct Lcg {
 }
 impl Lcg {
     fn next(&mut self) -> u64 {
-        self.state = self.state.wrapping_mul(6364136223846793005).wrapping_add(1442695040888963407);
+        self.state = self
+            .state
+            .wrapping_mul(6_364_136_223_846_793_005)
+            .wrapping_add(1_442_695_040_888_963_407);
         self.state
     }
 }
