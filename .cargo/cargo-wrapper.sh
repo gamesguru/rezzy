7183c2ceee94842cd1b96c6441b198a451bd2b56
Shane Jaroch <chown_tee@proton.me>
2026-07-03 08:28:28 -0400

    chore: add sccache + mold cargo wrapper

---
+++ .cargo/cargo-wrapper.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+RUSTC="$1"
+shift
+
+CMD=("$RUSTC")
+
+if [[ -z "$NO_SCCACHE" ]] && command -v sccache >/dev/null 2>&1; then
+	CMD=(sccache "$RUSTC")
+fi
+
+MOLD_ARGS=()
+if command -v mold >/dev/null 2>&1; then
+	# Do not use mold if cross-compiling to webassembly or riscv (SP1)
+	if [[ "$*" == *"wasm32"* ]] || [[ "$*" == *"riscv"* ]]; then
+		: # skip mold
+	else
+		MOLD_ARGS=("-C" "link-arg=-fuse-ld=mold")
+	fi
+fi
+
+exec "${CMD[@]}" "$@" "${MOLD_ARGS[@]}"
